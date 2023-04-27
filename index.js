c347133c1d965bfb3a75540ced12e578afcbbe8b
Alan Miguel Rocha <alanmrochadeveloper@gmail.com>
2023-04-27 11:57:03 -0300

    refactor: variable name changed to make it clearer

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const totalSumArray = (array) => {
   if (array.length < 1) return 0;
-  const firstItemValue = array.pop()
-  return firstItemValue + totalSumArray(array);
+  const lastItemValue = array.pop()
+  return lastItemValue + totalSumArray(array);
 }
 
 const input = [1, 2, 3, 4, 5]
